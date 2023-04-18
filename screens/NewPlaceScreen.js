a1d1041e460c661164bb418c401952c5009bf873
Mauro Vargas <mauroseba2000@gmail.com>
2023-04-18 15:24:18 -0300

    terminando tp SQlite

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -3,24 +3,24 @@ import { View, Text, StyleSheet, ScrollView, TextInput, Button } from 'react-nat
 import { COLORS } from '../constants'
 import { useDispatch } from 'react-redux'
 import { addPlace } from '../store/places.actions'
-import ImageSelector from '../components/ImageSelector' 
+import ImageSelector from '../components/ImageSelector'
+import LocationService from '../components/LocationService'
 
 
 const NewPlaceScreen = ({ navigation }) => {
     const dispatch = useDispatch()
     const [titleValue, setTitleValue] = React.useState('')
     const [imageValue, setImageValue] = React.useState('')
+    const [locationValue, setLocationValue] = React.useState()
 
     const titleChangeHandler = text => {
         setTitleValue(text)
     }
 
-
     const savePlaceHandler = () => {
-        dispatch(addPlace(titleValue,imageValue))
+        dispatch(addPlace(titleValue,imageValue, locationValue))
         navigation.navigate('Direcciones')
     }
-
 
     return (
         <ScrollView>
@@ -28,6 +28,7 @@ const NewPlaceScreen = ({ navigation }) => {
                 <Text style={styles.label}>Titulo</Text>
                 <TextInput style={styles.input} onChangeText={titleChangeHandler}/>
                 <ImageSelector onImage={image=>setImageValue(image)} />
+                <LocationService onLocation={(lat, lng)=>setLocationValue({lat, lng})}/>
                 <Button title="Guardar" color={COLORS.MAROON} onPress={savePlaceHandler} />
             </View>
            
